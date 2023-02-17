1d94d0a42c4a183724a520f665699c29d53a788c
nxt3d <pm@premmakeig.com>
2023-02-17 16:26:16 -0500

    add ExampleContract

---
+++ contracts/mocks/ExampleContract.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "../IXAPRegistry.sol";
+
+contract ExampleContract {
+
+    IXAPRegistry public xap;
+
+    // XAP names can be stored with any length of bytes e.g. Bytes10 for a 
+    // maximum ten character name. The maximum number of bytes is 32.
+
+    bytes10 public xapName;
+
+    constructor(IXAPRegistry _xap) {
+        xap = _xap;
+    }
+
+    function resolveXAPAddress() public view returns (address) {
+        return xap.resolveAddress(bytes32(xapName),1);
+    }
+
+    function setXAPName(bytes10 _name) public {
+        xapName = _name;
+    }
+}
